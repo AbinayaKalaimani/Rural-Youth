a491b731423cc4778798405dd69bb3667acd1bfb
ABINAYA K <askabinaya05@gmail.com>
2026-09-20 08:22:53 +0530

    Add files via upload

---
+++ Code/script.js
@@ -0,0 +1,162 @@
+// alert("Hello Welcome....!")
+
+
+// console.log("Test")
+//console.log("Add : " ,5+5)
+
+/*let myname = "Abi";
+document.getElementById("mynameelement").innerHTML = myname;*/
+
+// console.log(myname);
+
+
+// "Variable Declaration Method"
+
+/* let x = 5;
+x = 10;
+console.log(x); */
+
+/* var x = 5;
+x = 10;
+console.log(x); */
+
+/* let x = 5;
+let x = 10;
+console.log(x); */
+
+/* var x = 5;
+var x = 10;
+console.log(x); */
+
+/* const y = 5;
+y = 10;
+console.log(y); */
+
+
+// "Data Types"
+
+// let name = "Abinaya1511"; --> String
+
+// let number = 1511; --> Number
+
+/* let isloggedin = true; --> Boolean
+let isloggedin = false; --> Boolean*/
+
+/* let x;
+console.log(x); */
+
+
+// "Object Syntax"
+
+/* let Object{
+    Key : Value
+}
+
+let object = {
+    Name : "Abi",
+    city : "Attur",
+    contact : 6382134545
+}
+
+console.log(object.contact);
+console.log(object.city);
+console.log(typeof object.Name);
+console.log(typeof object.contact);
+ */
+
+// "Type Casting"
+
+/* let x = 5;
+let y = "5";
+console.log(x + y); */
+
+/* let x = "RY";
+let y = "Abi";
+console.log(x + y); */
+
+/* let x = "5";
+let y = 2;
+console.log(x + y); */
+
+/* let x = Number("5")
+let y = 2;
+console.log(x + y); */
+
+/* let x = Number("Abi")
+let y = 2;
+console.log(x + y); */
+
+/* let x = String("Abi")
+let y = 2;
+console.log(x + y); */
+
+
+//"Operators"
+//1.Arithmetic Operator
+/* console.log(15 + 11 + 5); --> Addition
+console.log(15 - 11); --> Subtraction
+console.log(20 * 20); --> Multiplication
+console.log(400 / 20); --> Division
+console.log(11 % 2); --> Modulus
+console.log(2 ** 3); --> Power */
+
+//2.Increment/Decrement Operator
+/* let x = 10;
+x++; //x = x + 1
+console.log(x);
+x--; //x = x - 1
+console.log(x); */
+
+//3.Assignment Operator
+/* let y = 10;
+y = y + 5;
+console.log(y);
+
+let x = 20;
+x+=5; --> add and assign
+console.log(x);
+x-=10; --> subtract and assign
+console.log(x);
+x*=2; --> multiply and assign
+console.log(x);
+x/=2;--> divide and assign
+console.log(x);
+ */
+
+//4.Comparision Operator
+/* let age = 18;
+console.log(age == 18); --> Double equal to
+console.log(age == 20); */
+
+/* let age = "18";
+console.log(age == 18); --> Double equal to
+console.log(age === 18); --> Triple equal to
+console.log(age != 18); --> Not equal to
+console.log(age !== 18); --> Not Double equal to
+console.log(age > 18); --> Greater than
+console.log(age >= 18); --> Greater than or equal to
+console.log(age < 19); --> Less than
+console.log(age <= 19); --> Less than or equal to */
+
+//5.Logical Operator
+/* let x = 18;
+let y = 20;
+console.log(x == 18 && y == 20); -->And
+console.log(x == 18 && y == 21); 
+console.log(x == 18 || y == 20); -->Or
+let t = true;
+console.log(!t); -->Not
+let f = false;
+console.log(!f); */
+
+//6.Ternary Operator
+/* let age = 18;
+let result = age >= 17 ? "College Student" : "School Student";
+console.log(result);
+
+let ages = 18;
+let results = ages >= 20 ? "College Student" : "School Student";
+console.log(results);
+ */
+
+//console.log(document);
